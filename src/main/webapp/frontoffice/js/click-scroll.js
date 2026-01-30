c8d96586fa77d6c9d67295b63933dcf518e3bc20
artango666 <artangosol@gmail.com>
2026-01-30 10:06:15 +0100

    initialisation projet hellmetz

---
+++ src/main/webapp/frontoffice/js/click-scroll.js
@@ -0,0 +1,54 @@
+//jquery-click-scroll
+//by syamsul'isul' Arifin
+
+var sectionArray = [1, 2, 3, 4, 5, 6];
+
+$.each(sectionArray, function(index, value){
+          
+     $(document).scroll(function(){
+         var offsetSection = $('#' + 'section_' + value).offset().top - 83;
+         var docScroll = $(document).scrollTop();
+         var docScroll1 = docScroll + 1;
+         
+        
+         if ( docScroll1 >= offsetSection ){
+             $('.navbar-nav .nav-item .nav-link').removeClass('active');
+             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
+             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+         }
+         
+     });
+    
+    $('.click-scroll').eq(index).click(function(e){
+        var offsetClick = $('#' + 'section_' + value).offset().top - 83;
+        e.preventDefault();
+        $('html, body').animate({
+            'scrollTop':offsetClick
+        }, 300)
+    });
+    
+});
+
+$(document).ready(function(){
+    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
+    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
+    $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
+});
+
+// PARTIE ANIMATION POUR ICONE DU COMPTEUR ET VILLE
+function updateDigit(el, value) {
+    if (el.textContent !== value) {
+        el.textContent = value;
+        el.classList.remove('animate');
+        void el.offsetWidth; 
+        el.classList.add('animate');
+    }
+}
+
+const digits = document.querySelectorAll('.time b');
+
+setInterval(() => {
+    const now = new Date();
+    updateDigit(digits[3], String(now.getSeconds()).padStart(2, '0'));
+}, 1000);
