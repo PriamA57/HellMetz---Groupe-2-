00090169e8c2a72ad309e6b26c12d36765046b6c
PriamA57 <priam.antoine57@gmail.com>
2026-04-02 14:25:52 +0200

    Résolution des conflits

--- src/main/java/com/hellmetz/festival/backoffice/model/Utilisateur.java
+++ src/main/java/com/hellmetz/festival/backoffice/model/Utilisateur.java
@@ -1,4 +1,35 @@
 package com.hellmetz.festival.backoffice.model;
 
 public class Utilisateur {
+    private Long idUtilisateur;
+    private String identifiant;
+    private String nom;
+    private String prenom;
+    private String email;
+
+    // Constructeur
+    public Utilisateur(Long idUtilisateur, String identifiant, String nom, String prenom, String email) {
+        this.idUtilisateur = idUtilisateur;
+        this.identifiant = identifiant;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+    // Getters et Setters
+    public Long getIdUtilisateur() { return idUtilisateur; }
+    public void setIdUtilisateur(Long idUtilisateur) { this.idUtilisateur = idUtilisateur; }
+
+    public String getIdentifiant() { return identifiant; }
+    public void setIdentifiant(String identifiant) { this.identifiant = identifiant; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
 }
