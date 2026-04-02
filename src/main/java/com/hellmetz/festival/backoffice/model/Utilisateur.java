defd258251fb5e036cb8222f72359ca6ecfcf713
PriamA57 <priam.antoine57@gmail.com>
2026-04-02 16:08:59 +0200

    Modification du Utilisateur.java

--- src/main/java/com/hellmetz/festival/backoffice/model/Utilisateur.java
+++ src/main/java/com/hellmetz/festival/backoffice/model/Utilisateur.java
@@ -6,14 +6,20 @@ public class Utilisateur {
     private String nom;
     private String prenom;
     private String email;
+    private boolean actif;
+    private Long date_creation;
+    private Long derniere_connexion;
 
     // Constructeur Utilisateur !!
-    public Utilisateur(Long idUtilisateur, String identifiant, String nom, String prenom, String email) {
+    public Utilisateur(Long idUtilisateur, String identifiant, String nom, String prenom, String email, boolean actif, Long date_creation, Long derniere_connexion) {
         this.idUtilisateur = idUtilisateur;
         this.identifiant = identifiant;
         this.nom = nom;
         this.prenom = prenom;
         this.email = email;
+        this.actif = actif;
+        this.date_creation = date_creation;
+        this.derniere_connexion = derniere_connexion;
     }
 
     // Getters et Setters
@@ -32,4 +38,12 @@ public class Utilisateur {
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
+    public boolean getActif() { return actif; }
+    public void setActif(boolean actif) { this.actif = actif; }
+
+    public Long getDate_creation() { return date_creation; }
+    public void setDate_creation(Long date_creation) { this.date_creation = date_creation; }
+
+    public Long getDerniere_connexion() { return derniere_connexion; }
+    public void setDerniere_connexion(Long derniere_connexion) { this.derniere_connexion = derniere_connexion; }
 }
